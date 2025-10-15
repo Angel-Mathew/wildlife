d403932f8e52416b235e56fbeb8614a3008e4232
Angel-Mathew <144330644+Angel-Mathew@users.noreply.github.com>
2025-10-15 19:38:21 +0400

    updated

--- src/assets/data/location.js
+++ src/assets/data/location.js
@@ -3,8 +3,8 @@ const locationdata={
     "Asia":[{
       "id":"asia",
       "name":"Asia",
-       "image":"src/assets/map/asia.png",
-       "lvingbeing":"🐎 Horse,🐅 Tiger,🐘 Elephant,🦏 Rhinoceros,𓃵 Markhor,𓃲 Arabian Oryx,🐪 Camel,ʕ•ᴥ•ʔ Red Panda,🐺 Wolf,Beaver,🦌 Sika Deer,🐃 The Carabao,🦦 Otter,🦇 Bat,🐸 Frog,🦗 Grasshopper,🕷️ Spider,🐌 Snail,🦋 Butterfly,🐝 Honeybee,🐞 LadyBug,🐊 Crocodile,🦎 Gecko,🦎 Chameleon,🦎 Monitor lizards, 🐍 Python, 🐍 Cobra,🐢 Tortoise,🐢 Turtle,🦎 Lizard,🦎 Skink,🦜 Parrot,🦅 Eagle,🦚 Peacock,🦩 Flamingo,𓅚 Woodpecker,🐦 Canary,🦢 Swan,🕊 Hummingbird,𓄿 Vulture,𓅓 Hawk,🦜 Cockatoo,🦜 Macaw,🐦 Hornbill,🦆 Duck,𓅇 Falcon,🐓 Hen,🐦 cuckoo,🐦 Pheasant,🐦 Hoopoe,Philippine eagle ,𓅚 Lapwing,𓅯 kingfisher,🐦‍⬛ Black drongo,🐦‍⬛ Crow,🕊️ Dove,🐟 Queen Angelfish,🐡 Pufferfish,🐠 Blue Tang,🦈 Shark,Seahorse,Clownfish,Stingray,Grouper,Lionfish,Swordfish,Siamese Fighting Fish,Goldfish,Catfish,Skate Fish,Butterfly Fish,Barracuda ",
+       "image":"src/assets/maps/asia.png",
+       "animals":" Horse,Tiger,Elephant,Rhinoceros,Markhor,Arabian Oryx,Camel,Red Panda,Wolf,Beaver,Sika Deer,The Carabao,Otter,Bat,Frog, Grasshopper, Spider,Snail,Butterfly,Honeybee,LadyBug,Crocodile,Gecko,Chameleon,Monitor lizards,Python,Cobra,Tortoise,Turtle,Lizard,Skink,Parrot,Eagle,Peacock,Flamingo,Woodpecker,Canary, Swan,Hummingbird,Vulture,Hawk,Cockatoo,Macaw,Hornbill,Duck,Falcon,Hen,cuckoo,Pheasant,Hoopoe,Philippine eagle,Lapwing,kingfisher,Black drongo,Crow,Dove,Queen Angelfish, Pufferfish,🐠 Blue Tang,🦈 Shark,Seahorse,Clownfish,Stingray,Grouper,Lionfish,Swordfish,Siamese Fighting Fish,Goldfish,Catfish,Skate Fish,Butterfly Fish,Barracuda ",
        "details_api_endpoint":"/api/locationdata/asia",
     }
         
@@ -13,26 +13,26 @@ const locationdata={
   "Africa":[{
     "id":"africa",
     "name":"Africa",
-    "image":"src/assets/map/africa.png",
-    "livingbeing":"🐘 Elephant,🦏 Rhinoceros,🦒 Giraffe,🦓 Zebra,🦁 Lion,🐒 Chimpanzee,🦡 Honey Badger,Meerkat,Cheetah,Caracal,🐊 Crocodile,🦎 Gecko,🦎 Chameleon,🦎 Monitor lizards, 🐍 Python, 🐍 Cobra,🐢 Tortoise,🐢 Turtle,🦎 Lizard,🦎 Skink, 🦜 Parrot,🦅 Eagle,🦚 Peacock,🦩 Flamingo,𓅚 Woodpecker,𓄿 Vulture,𓅓 Hawk,🐦 Hornbill,🦆 Duck,𓅇 Falcon,🐦 cuckoo,🐦 Hoopoe,𓅯 kingfisher,🐦‍⬛ Crow,🕊️ Dove,🦉Owl,Spoonbill,Ostrich,🐡 Pufferfish,🦈 Shark,Seahorse,Stingray,Grouper,Swordfish,Catfish,Skate fish,Butterfly fish,Barracuda ",
+    "image":"src/assets/maps/africa.png",
+    "animals":"Elephant,Rhinoceros,Giraffe,Zebra,Lion,Chimpanzee,Honey Badger,Meerkat,Cheetah,Caracal,Crocodile,Gecko,Chameleon,Monitor lizards, Python, Cobra,Tortoise,Turtle,Lizard,Skink, Parrot,Eagle,Peacock,Flamingo,Woodpecker,Vulture,Hawk,Hornbill,Duck,Falcon,cuckoo,Hoopoe,kingfisher,Crow, Dove,Owl,Spoonbill,Ostrich,Pufferfish,Shark,Seahorse,Stingray,Grouper,Swordfish,Catfish,Skate fish,Butterfly fish,Barracuda ",
     "details_api_endpoint":"/api/locationdata/africa",
 }
 ],
 /*------------------------------------ South America -------------------------------------------*/
 "South_America":[{
     "id":"south_america",
-    "name":"South_america",
-    "image":"src/assets/map/south_america.png",
-    "livingbeing":"Horse,Capybare,Sloth,Jaguar,Armadillo,Otter,Bat,Frog,sealion,Dolphin,Grasshopper,Spider,Snail,Butterfly,Ladybug,Crocodile,Iguana,Monitor lizards,Python,Cobra,Tortoise,Turtle,Lizard,Skink,Anole,Parrot,Eagle,Owl,Penguin,Flamingo,Woodpecker,Toucan,Hummingbird,Vulture,Hawk,Albatross,Macaw,Duck,Falcon,Cuckoo,Puffin,Spoonbill,Kingfisher,Crow,Dove,Seahorse,Pufferfish,Shark,Stingray,Lionfish,Swordfish,Electric Eel,Catfish,Skate Fish,Piranha,Barracuda",
-     "details_api_endpoint":"/api/locationdata/south_america",
+    "name":"South America",
+    "image":"src/assets/maps/south_america.png",
+    "animals":"Horse,Capybare,Sloth,Jaguar,Armadillo,Otter,Bat,Frog,sealion,Dolphin,Grasshopper,Spider,Snail,Butterfly,Ladybug,Crocodile,Iguana,Monitor lizards,Python,Cobra,Tortoise,Turtle,Lizard,Skink,Anole,Parrot,Eagle,Owl,Penguin,Flamingo,Woodpecker,Toucan,Hummingbird,Vulture,Hawk,Albatross,Macaw,Duck,Falcon,Cuckoo,Puffin,Spoonbill,Kingfisher,Crow,Dove,Seahorse,Pufferfish,Shark,Stingray,Lionfish,Swordfish,Electric Eel,Catfish,Skate Fish,Piranha,Barracuda",
+    "details_api_endpoint":"/api/locationdata/south_america",
 }
 ],
 /*------------------------------------ North America -------------------------------------------*/
 "North_America":[{
   "id":"north_america",
-    "name":"North_America",
-    "image":"src/assets/map/north_america.png",
-    "livingbeing":"Horse,Rabbit,Dog,Cat,Cougar,Coyote,Wolf,sealion ,Beaver,Armadillo,Squirrel,Otter,Bat,Frog,Dolphin,Grasshopper,Spider,Snail,Butterfly,Honeybee,Ladybug,Crocodile,Alligator,Gecko,Iguana,Chameleon,Monitor lizards,Python,Tortoise,Turtle,Lizard,Skink,Anole,Gila Monster,Parrot,Eagle,Owl,Woodpecker,Swan,Hummingbird,Vulture,Robin,Hawk,Duck,Falcon,Hens,Cuckoo,Puffin,Spoonbill,Blue Jay,Crow,Dove, Queen Angelfish, Pufferfish,  Shark,Seahorse,Clownfish,Stingray,Grouper,Lionfish,Swordfish,Goldfish,Catfish,Skate Fish,Barracuda ",
+    "name":"North America",
+    "image":"src/assets/maps/north_america.png",
+    "animals":"Horse,Rabbit,Dog,Cat,Cougar,Coyote,Wolf,sealion ,Beaver,Armadillo,Squirrel,Otter,Bat,Frog,Dolphin,Grasshopper,Spider,Snail,Butterfly,Honeybee,Ladybug,Crocodile,Alligator,Gecko,Iguana,Chameleon,Monitor lizards,Python,Tortoise,Turtle,Lizard,Skink,Anole,Gila Monster,Parrot,Eagle,Owl,Woodpecker,Swan,Hummingbird,Vulture,Robin,Hawk,Duck,Falcon,Hens,Cuckoo,Puffin,Spoonbill,Blue Jay,Crow,Dove, Queen Angelfish, Pufferfish,  Shark,Seahorse,Clownfish,Stingray,Grouper,Lionfish,Swordfish,Goldfish,Catfish,Skate Fish,Barracuda ",
     "details_api_endpoint":"/api/locationdata/north_america",
 }
 ],
@@ -40,8 +40,8 @@ const locationdata={
 "Antarctica":[{
     "id":"antarctica",
     "name":"Antarctica",
-    "image":"src/assets/map/antarctica.png",
-    "livingbeing":"Penguin,Albatross,Skate fish,Sharks  "            ,
+    "image":"src/assets/maps/antarctica.png",
+    "animals":"Penguin,Albatross,Skate fish,Sharks  "            ,
     "details_api_endpoint":"/api/locationdata/antarctica",
 }
 ],
@@ -49,8 +49,8 @@ const locationdata={
 "Australia":[{
     "id":"australia",
     "name":"Australia",
-    "image":"src/assets/map/australia.png",
-    "livingbeing":"Kangaroo,Koala,Rabbit,Dog,Cat,Dolphin,sealion ,Bat,Spider,Snail,Butterfly,Sugar Glider,Honeybee,Ladybug,Duck,Crocodile,Gecko,Monitor Lizards,Python,Turtle,Lizard,Skink,Parrot,Eagle,Owl,Penguin,Peacock,Flamingo,Swan,Robin,Hawk,Albatross,Cockatoo,Duck,Falcon,Hen,Cuckoo,Pheasant,Spoonbill,Emu,Lapwing,Kingfisher,Seahorse,Clownfish,Pufferfish,Shark,Stingray,Grouper,Lionfish,Swordfish,Goldfish,Catfish,Skate Fish,Blue Tang,Butterfly Fish,Barracuda",
+    "image":"src/assets/maps/australia.png",
+    "animals":"Kangaroo,Koala,Rabbit,Dog,Cat,Dolphin,sealion ,Bat,Spider,Snail,Butterfly,Sugar Glider,Honeybee,Ladybug,Duck,Crocodile,Gecko,Monitor Lizards,Python,Turtle,Lizard,Skink,Parrot,Eagle,Owl,Penguin,Peacock,Flamingo,Swan,Robin,Hawk,Albatross,Cockatoo,Duck,Falcon,Hen,Cuckoo,Pheasant,Spoonbill,Emu,Lapwing,Kingfisher,Seahorse,Clownfish,Pufferfish,Shark,Stingray,Grouper,Lionfish,Swordfish,Goldfish,Catfish,Skate Fish,Blue Tang,Butterfly Fish,Barracuda",
     "details_api_endpoint":"/api/locationdata/australia",
 }
 ],
@@ -58,8 +58,8 @@ const locationdata={
 "Europe":[{
     "id":"europe",
     "name":"Europe",
-    "image":"src/assets/map/europe.png",
-    "livingbeing":"Horse,Rabbit,Dog,Cat,Lion,Cow,Sheep,Cheetah,Wolf,Beaver,Sika Deer,Squirrel,Otter,Bat,Frog,Dolphin,Grasshopper,Spider,Snail,Butterfly,Honeybee,Ladybug,Gecko,Chameleon,Tortoise,Turtle,Lizard,Eagle, Owl, Flamingo, Woodpecker, Swan, Vulture, Robin, Hawk, Duck, Falcon, Hen, Cuckoo, Peacock, Puffin, Spoonbill,Chukar Partridge,Hoopoe,Lapwing,Kingfisher,Crow,Dove,Seahorse,Shark,Stingray,Grouper,Swordfish,Catfish,Skate Fish,Barracuda",
+    "image":"src/assets/maps/europe.png",
+    "animals":"Horse,Rabbit,Dog,Cat,Lion,Cow,Sheep,Cheetah,Wolf,Beaver,Sika Deer,Squirrel,Otter,Bat,Frog,Dolphin,Grasshopper,Spider,Snail,Butterfly,Honeybee,Ladybug,Gecko,Chameleon,Tortoise,Turtle,Lizard,Eagle, Owl, Flamingo, Woodpecker, Swan, Vulture, Robin, Hawk, Duck, Falcon, Hen, Cuckoo, Peacock, Puffin, Spoonbill,Chukar Partridge,Hoopoe,Lapwing,Kingfisher,Crow,Dove,Seahorse,Shark,Stingray,Grouper,Swordfish,Catfish,Skate Fish,Barracuda",
     "details_api_endpoint":"/api/locationdata/europe",
 }
 ],
--- src/pages/Sightings.jsx
+++ src/pages/Sightings.jsx
@@ -1,11 +1,82 @@
-import React, { useState } from 'react'; // <--- Make sure useState is imported
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import locationdata from '../assets/data/location';
 import './Sightings.css';
 import Navbar from '../components/Navbar';
 
-const Sightings=({
-  
-})
+const Sightingscard = ({ name, image, onClick }) => {
+    return (
+        
+        <div className='Sightingscard' onClick={onClick}>
+            <img src={image} alt={name} className='map'/>
+            <h3 className='Sightingname'>{name}</h3>
+           
+        </div>
+    );
+};
 
-export default Sightings; // This is the correct default export
+// Component for the dropdown menu
+const DropdownMenu = ({ animals }) => { // Prop is 'animals'
+    // Ensure 'animals' is not null/empty
+    if (!animals || animals.length === 0) {
+        return <div className="dropdown-content">No animals listed.</div>;
+    }
+
+    // Split the comma-separated string into an array, trim whitespace
+    const animalsList = animals.split(',').map(item => item.trim()); // Correctly creates the array
+
+    return (
+        <div className="dropdown-content">
+            <ul>
+               
+                {animalsList.map((item, index) => {
+                    const parts = item.split(' ');
+                    const emoji = parts[0];
+                    const name = parts.slice(1).join(' ');
+
+                    return (
+                        <li key={index} className="dropdown-item">
+                            {emoji && <span className="animal-emoji">{emoji}</span>}
+                            <span className="animal-name">{name}</span>
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
+    );
+};
+
+// Main Sightings component
+const Sightings = () => {
+    const [activeContinent, setActiveContinent] = useState(null);
+
+    const handleCardClick = (continentName) => {
+        setActiveContinent(prevActive => (prevActive === continentName ? null : continentName));
+    };
+
+    const continents = Object.values(locationdata).map(arr => arr[0]);
+
+    return (
+        <div className="sightings-page">
+            <Navbar />
+            <h1>Sightings by Continent</h1>
+            <div className="sightings-grid">
+                {continents.map((continent) => (
+                    <div key={continent.id} className="continent-wrapper">
+                        <Sightingscard
+                            name={continent.name}
+                            image={continent.image}
+                            onClick={() => handleCardClick(continent.name)}
+                        />
+                        
+                        {activeContinent === continent.name && (
+                            <DropdownMenu animals={continent.animals} /> // This is correct now
+                        )}
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Sightings;
